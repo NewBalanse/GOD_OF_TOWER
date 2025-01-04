daeefe204df490038de68fa9211f9a750ed247da
NewBalanse <coocies12@gmail.com>
2025-01-05 01:10:32 +0200

    master commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+// constants
+const manifestFileUrl: string = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+
+createRoot(document.getElementById('root')!).render(
+  <TonConnectUIProvider manifestUrl={manifestFileUrl}>
+    <App/>
+  </TonConnectUIProvider>,
+)
